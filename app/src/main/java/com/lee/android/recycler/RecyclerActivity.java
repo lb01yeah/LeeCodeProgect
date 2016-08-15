bcd2317cd057728b91436670d4008d10d2d958a7
lb01yeah <lb01yeah@yeah.net>
2016-08-15 22:51:30 +0800

    RecyclerView 1.1

--- app/src/main/java/com/lee/android/recycler/RecyclerActivity.java
+++ app/src/main/java/com/lee/android/recycler/RecyclerActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.MotionEvent;
 
 import com.lee.android.R;
 import com.lee.android.recycler.bean.ContentInfo;
@@ -24,6 +25,7 @@ public class RecyclerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recycler);
 
         initData();
+        initView();
     }
 
     private void initData() {
@@ -59,29 +61,69 @@ public class RecyclerActivity extends AppCompatActivity {
 
             @Override
             public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
-                return 0;
+                //actionState : action状态类型，有三类 ACTION_STATE_DRAG （拖曳），ACTION_STATE_SWIPE（滑动），ACTION_STATE_IDLE（静止）
+                int dragFlags = makeFlag(ItemTouchHelper.ACTION_STATE_DRAG,ItemTouchHelper.UP|ItemTouchHelper.DOWN
+                |ItemTouchHelper.LEFT |ItemTouchHelper.RIGHT); //支持上下左右的拖拽
+                int swipFlags = makeMovementFlags(ItemTouchHelper.ACTION_STATE_SWIPE,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT);//支持左右的滑动
+
+                return makeMovementFlags(dragFlags,swipFlags); //返回0，表示不支持拖拽和滑动
+
             }
 
+            /**
+             * @param recyclerView attach的RecyclerView
+             * @param viewHolder 拖动的Item
+             * @param target 放置Item的目标位置
+             * @return
+             */
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                return false;
+                int fromPosition= viewHolder.getAdapterPosition(); //要拖拽的位置
+                int toPosition = target.getAdapterPosition(); //要放置的目标位置
+                mRecyclerAdapter.moveItem(fromPosition,toPosition);
+                return true;
             }
 
+            /**
+             * @param viewHolder 滑动移除的Item
+             * @param direction
+             */
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();//获取要滑动删除的Item的位置
+                mRecyclerAdapter.removeItem(position);
 
             }
 
             @Override
             public boolean isLongPressDragEnabled() {
-                return super.isLongPressDragEnabled();
+                return super.isLongPressDragEnabled(); //不支持长按拖拽效果直接返回false
             }
 
             @Override
             public boolean isItemViewSwipeEnabled() {
-                return super.isItemViewSwipeEnabled();
+                return super.isItemViewSwipeEnabled(); //不支持滑动效果直接返回false
             }
         });
+
+        itemTouchHelper.attachToRecyclerView(mRecyclerView);
+        mRecyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
+
 
     }
 
